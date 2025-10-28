807b70cc3f10b4635c519c04ea6b472e9bd77c17
wolfron <12mell31@gmail.com>
2025-10-28 23:19:32 +0200

    changes with comments

--- nutritions.cpp
+++ nutritions.cpp
@@ -1,9 +1,6 @@
 #include "nutritions.h"
 #include <fstream>
-#include <sstream>
 #include <string>
-#include <unordered_map>
-#include <iostream>
 
 nutritions::nutritions(const Meal& meal, double g) 
     : meal_info(meal), meal_weight(g) {}
@@ -21,12 +18,17 @@ double nutritions::get_fat() const{
 double nutritions::get_carbs() const{ 
     return meal_info.carbs * meal_weight / 100.0; 
 }
+double nutritions::get_meal_weight() const{ 
+    return meal_weight; 
+}
 
 Meal Meal::find_dish(const std::string& dish_find) {
     std::fstream file("meals_list.txt");
     std::string dish_name;
     double protein, fat, carbs;
-
+    if(dish_name == "Water"){
+        return Meal("Water", 0, 0, 0);
+	}
     while (file >> dish_name >> protein >> fat >> carbs) {
         if (dish_name == dish_find) {
             return Meal(dish_name, protein, fat, carbs);
