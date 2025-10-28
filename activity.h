7e4aa007c4cb91af6d3460a36c00d5faba6a1a30
nastesi <anastasadola.ua@gmail.com>
2025-10-28 23:24:26 +0200

    changed activity files for ongoing work on manager

--- activity.h
+++ activity.h
@@ -1,48 +1,25 @@
 #pragma once
 #include <string>
-#include <vector>
 
-enum class ExerciseType {
-    Running, 
-    Swimming, 
-    Cycling, 
-    Walking, 
-    StrengthTraining, 
-    Yoga, 
-    Dancing,
-    Hiking,
-    Aerobics,
-    Boxing,
-    Rowing,
-    JumpRope,
-    Basketball,
-    Football,
-    Pilates,
-    Tennis,
-    Skating,
-    Skiing,
-    Stretching,
-    StairClimbing,
-    Crossfit,
-    HouseCleaning,
-    Gardening,
-    Climbing
+class ActivityInfo {
+public:
+    std::string name;
+    double MET;
+
+    ActivityInfo() = default;
+    ActivityInfo(const std::string& n, double met);
+
+    static ActivityInfo find_activity(const std::string& activity_name);
 };
 
 class activity {
 public:
-    ExerciseType type;
-    double durationMin;
-    double weight;
+    ActivityInfo info;
+    double durationMin{}; 
+    double weight{};      
 
     activity() = default;
+    activity(const ActivityInfo& i, double d, double w);
+
     double calculate_calories() const;
-
-    std::string type_to_string() const;
-    static ExerciseType string_to_type(const std::string& str);
-
-    void save_to_file(const std::string& filename) const;
-    static std::vector<activity> get_from_file(const std::string& filename, const std::string& date);
-
-    static double get_daily_burned_calories(const std::string& filename, const std::string& date);
 };
