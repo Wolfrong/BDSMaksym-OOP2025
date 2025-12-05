bcda4727f3de1ff2d5ba014afd549e46e24ef968
wolfron <12mell31@gmail.com>
2025-12-05 17:30:47 +0200

    lab4

--- nutritions.cpp
+++ nutritions.cpp
@@ -1,48 +1,44 @@
 #include "nutritions.h"
 #include <fstream>
-#include <string>
-
-double nutritions::get_calories() const { return meal->get_calories() * weight / 100.0; }
-double nutritions::get_proteins() const { return meal->get_proteins() * weight / 100.0; }
-double nutritions::get_fats() const { return meal->get_fats() * weight / 100.0; }
-double nutritions::get_carbs() const { return meal->get_carbs() * weight / 100.0; }
-double nutritions::get_meal_weight() const { return weight; }
+#include <iostream>
 
 Dishes Dishes::Dish(const std::string& dish_find) {
-    std::fstream file("meals_list.txt");
-    std::string name;
-    double protein, fat, carbs;
-    if (dish_find == "Water") {
-        return Dishes("Water", 0, 0, 0);
-    }
+    std::ifstream file("meals_list.txt");
 
-    while (file >> name >> protein >> fat >> carbs) {
+    std::string name;
+    double proteins, fats, carbs;
+
+    if (dish_find == "Water")
+        return Dishes("Water", 0, 0, 0);
+
+    while (file >> name >> proteins >> fats >> carbs) {
         if (name == dish_find) {
-            return Dishes(name, protein, fat, carbs);
+            return Dishes(name, proteins, fats, carbs);
         }
     }
+
     return Dishes("Blank", 0, 0, 0);
 }
 
 Dishes Dishes::Ingredient(const std::string& ingredient_find) {
     std::ifstream file("ingredients_list.txt");
-    std::string name;
-    double protein, fat, carbs;
 
-    while (file >> name >> protein >> fat >> carbs) {
+    std::string name;
+    double proteins, fats, carbs;
+
+    while (file >> name >> proteins >> fats >> carbs) {
         if (name == ingredient_find) {
-            return Dishes(name, protein, fat, carbs);
+            return Dishes(name, proteins, fats, carbs);
         }
     }
+
     return Dishes("Blank", 0, 0, 0);
 }
 
 void Self_Cook::add_ingredient(const std::string& ingredient_name, double grams) {
-    Dishes ingredient = Dishes::Ingredient(ingredient_name);
-    components.push_back({ ingredient, grams });
+    Dishes ing = Dishes::Ingredient(ingredient_name);
+    components.push_back({ ing, grams });
 }
-
-std::string Self_Cook::get_name() const { return name; }
 
 double Self_Cook::get_proteins() const {
     double sum = 0;
@@ -60,7 +56,7 @@ double Self_Cook::get_fats() const {
 
 double Self_Cook::get_carbs() const {
     double sum = 0;
-    for (auto& comps : components)
-        sum += comps.ingredient.get_carbs() * comps.grams / 100.0;
+    for (auto& c : components)
+        sum += c.ingredient.get_carbs() * c.grams / 100.0;
     return sum;
 }
--- nutritions.h
+++ nutritions.h
@@ -11,7 +11,10 @@ public:
     virtual double get_proteins() const = 0;
     virtual double get_fats() const = 0;
     virtual double get_carbs() const = 0;
-    virtual double get_calories() const { return get_proteins() * 4 + get_fats() * 9 + get_carbs() * 4; }
+
+    virtual double get_calories() const {
+        return get_proteins() * 4 + get_fats() * 9 + get_carbs() * 4;
+    }
 };
 
 class Dishes : public Meal {
@@ -22,7 +25,9 @@ private:
     double carbs;
 
 public:
-    Dishes(const std::string& n, double p, double f, double c) : name(n), protein(p), fat(f), carbs(c) {}
+    Dishes(const std::string& names, double proteins, double fats, double carb)
+        : name(names), protein(proteins), fat(fats), carbs(carb) {
+    }
 
     std::string get_name() const override { return name; }
     double get_proteins() const override { return protein; }
@@ -30,7 +35,7 @@ public:
     double get_carbs() const override { return carbs; }
 
     static Dishes Dish(const std::string& name);
-	static Dishes Ingredient(const std::string& name);
+    static Dishes Ingredient(const std::string& name);
 };
 
 class Self_Cook : public Meal {
@@ -46,26 +51,68 @@ private:
 
 public:
     Self_Cook(const std::string& n) : name(n) {}
-    ~Self_Cook() override = default;
 
     void add_ingredient(const std::string& ingredient_name, double grams);
-    std::string get_name() const override;
+
+    std::string get_name() const override { return name; }
+
     double get_proteins() const override;
     double get_fats() const override;
     double get_carbs() const override;
 };
 
-class nutritions {
+class NutritionDecorator : public Meal {
 private:
-    Meal* meal;
+    Meal* wrappee;
     double weight;
 
 public:
-    nutritions(Meal* m, double g = 100) : meal(m), weight(g) {}
+    NutritionDecorator(Meal* m, double w = 100) : wrappee(m), weight(w) {}
 
-    double get_calories() const;
-    double get_proteins() const;
-    double get_fats() const;
-    double get_carbs() const;
-	double get_meal_weight() const;
+    std::string get_name() const override { return wrappee->get_name(); }
+
+    double get_proteins() const override {
+        return wrappee->get_proteins() * weight / 100.0;
+    }
+
+    double get_fats() const override {
+        return wrappee->get_fats() * weight / 100.0;
+    }
+
+    double get_carbs() const override {
+        return wrappee->get_carbs() * weight / 100.0;
+    }
+
+    double get_calories() const override {
+        return Meal::get_calories();
+    }
+
+    double get_weight() const { return weight; }
+};
+
+class MealFactory {
+public:
+    virtual Meal* create(const std::string& name) = 0;
+    virtual ~MealFactory() = default;
+};
+
+class DishFactory : public MealFactory {
+public:
+    Meal* create(const std::string& name) override {
+        return new Dishes(Dishes::Dish(name));
+    }
+};
+
+class IngredientFactory : public MealFactory {
+public:
+    Meal* create(const std::string& name) override {
+        return new Dishes(Dishes::Ingredient(name));
+    }
+};
+
+class SelfCookFactory : public MealFactory {
+public:
+    Meal* create(const std::string& name) override {
+        return new Self_Cook(name);
+    }
 };
