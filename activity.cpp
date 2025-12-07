0ec4bdef4b96be413aba024e6d9b195220eed807
nastesi <anastasadola.ua@gmail.com>
2025-12-07 22:25:55 +0200

    edited

--- activity.cpp
+++ activity.cpp
@@ -1,53 +1,32 @@
 #include "activity.h"
 #include <fstream>
-#include <sstream>
 #include <iostream>
 
 ActivityInfo::ActivityInfo(const std::string& n, double met)
-    : name(n), MET(met) {
-}
+    : name(n), MET(met) {}
 
 ActivityInfo ActivityInfo::find_activity(const std::string& activity_name) {
     std::ifstream file("activity_MET.txt");
-    std::string n;
+    std::string name;
     double met;
 
-    while (file >> n >> met)
-        if (n == activity_name)
-            return ActivityInfo(n, met);
+    if (!file.is_open()) {
+        return ActivityInfo(activity_name, 1.0);
+    }
 
+    while (file >> name >> met) {
+        if (name == activity_name) {
+            return ActivityInfo(name, met);
+        }
+    }
+    std::cerr << "Warning: activity '" << activity_name
+        << "' not found. Using default MET = 1.0\n";
     return ActivityInfo(activity_name, 1.0);
 }
 
 activity::activity(const ActivityInfo& i, double d, double w)
     : info(i), durationMin(d), weightKg(w)
 {
-    assignStrategy();
-}
-
-void activity::assignStrategy() {
-    std::string n = info.name;
-
-    if (n == "Running" || n == "Cycling" || n == "Swimming" ||
-        n == "Hiking" || n == "Rowing" || n == "JumpRope")
-    {
-        calcStrategy = [](double dur, double w, double MET) {
-            return dur * MET * w * 0.0200;
-            };
-        return;
-    }
-
-    if (n == "StrengthTraining" || n == "Crossfit" || n == "Boxing")
-    {
-        calcStrategy = [](double dur, double w, double MET) {
-            return dur * w * 0.0300;
-            };
-        return;
-    }
-
-    calcStrategy = [](double dur, double w, double MET) {
-        return dur * MET * w * 0.0175;
-        };
 }
 
 std::string activity::getName() const {
@@ -55,10 +34,15 @@ std::string activity::getName() const {
 }
 
 double activity::calculate(double durationMin, double weightKg) const {
-    return calcStrategy(durationMin, weightKg, info.MET);
+    // беремо MET передаємо у змінний hook-метод
+    return calcFormula(durationMin, weightKg, info.MET);
 }
 
 double activity::calculate() const {
-    return calcStrategy(durationMin, weightKg, info.MET);
+    return calcFormula(durationMin, weightKg, info.MET);
 }
 
+// (раніше: Cardio / Strength / Simple через switch)
+double activity::calcFormula(double duration, double weight, double MET) const {
+    return duration * MET * weight * 0.0175;
+}
--- activity.h
+++ activity.h
@@ -1,8 +1,6 @@
 #pragma once
 #include <string>
-#include <functional>
 
-// inheritance changed to strategy type pattern
 class ActivityInfo {
 public:
     std::string name;
@@ -14,23 +12,32 @@ public:
     static ActivityInfo find_activity(const std::string& activity_name);
 };
 
-class activity {
+class ActivityBase {
 public:
-    ActivityInfo info;
+    virtual ~ActivityBase() = default;
+
+    virtual std::string getName() const = 0;
+
+    // Template Method
+    virtual double calculate(double durationMin, double weightKg) const = 0;
+};
+
+class activity : public ActivityBase {
+public:
+    ActivityInfo info;    
     double durationMin{};
     double weightKg{};
 
-private:
-    std::function<double(double, double, double)> calcStrategy;
-
-public:
     activity() = default;
     activity(const ActivityInfo& i, double d, double w);
 
-    std::string getName() const;
-    double calculate(double durationMin, double weightKg) const;
+    std::string getName() const override;
+
+    // Template Method
+    double calculate(double durationMin, double weightKg) const override;
     double calculate() const;
 
 private:
-    void assignStrategy();  // вибір стратегії
+    // Hook
+    double calcFormula(double duration, double weight, double MET) const;
 };
